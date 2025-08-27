ecb5a81340b5f2c83e0ba2e8b8895f639e5d1d1c
Felipe Araujo <141876774+FelipeGA02@users.noreply.github.com>
2025-08-27 20:17:44 -0300

    .

--- src/game/FlappyBird.py
+++ src/game/FlappyBird.py
@@ -1,6 +1,10 @@
 import pygame
 import os
 import random
+import neat
+
+ai_playing = True
+generation = 0
 
 ground_image = pygame.transform.scale2x(pygame.image.load(os.path.join('assets', 'base.png')))
 background_image = pygame.transform.scale2x(pygame.image.load(os.path.join('assets', 'bg.png')))
@@ -16,10 +20,10 @@ window_height = 800
 
 pygame.font.init()
 score_font = pygame.font.SysFont('roboto', 20)
+
+
 class Bird:
-    
     birdImages = bird_images
-    
     rotation = 25
     rotation_speed = 20
     animation_time = 5
@@ -41,16 +45,16 @@ class Bird:
 
     def move(self):
         self.time += 1
-        range = 1.5 * (self.time**2) + self.speed * self.time
+        displacement = 1.5 * (self.time ** 2) + self.speed * self.time
 
-        if range > 16:
-            range = 16
-        elif range < 0:
-            range -= 2
+        if displacement > 16:
+            displacement = 16
+        elif displacement < 0:
+            displacement -= 2
 
-        self.y += range
+        self.y += displacement
 
-        if range < 0 or self.y < (self.height + 50):
+        if displacement < 0 or self.y < (self.height + 50):
             if self.angle < self.rotation:
                 self.angle = self.rotation
         else:
@@ -62,19 +66,19 @@ class Bird:
 
         if self.bird_image_index < self.animation_time:
             self.image = self.birdImages[0]
-        elif self.bird_image_index < self.animation_time*2:
+        elif self.bird_image_index < self.animation_time * 2:
             self.image = self.birdImages[1]
-        elif self.bird_image_index < self.animation_time*3:
+        elif self.bird_image_index < self.animation_time * 3:
             self.image = self.birdImages[2]
-        elif self.bird_image_index < self.animation_time*4:
+        elif self.bird_image_index < self.animation_time * 4:
             self.image = self.birdImages[1]
-        elif self.bird_image_index >= self.animation_time*4 + 1:
+        elif self.bird_image_index >= self.animation_time * 4 + 1:
             self.image = self.birdImages[0]
             self.bird_image_index = 0
 
         if self.angle <= -80:
             self.image = self.birdImages[1]
-            self.bird_image_index = self.animation_time*2
+            self.bird_image_index = self.animation_time * 2
 
         rotation_image = pygame.transform.rotate(self.image, self.angle)
         after_center_image = self.image.get_rect(topleft=(self.x, self.y)).center
@@ -84,8 +88,9 @@ class Bird:
     def get_mask(self):
         return pygame.mask.from_surface(self.image)
 
+
 class Pipe:
-    range = 200
+    range = 160
     speed = 5
 
     def __init__(self, x):
@@ -104,13 +109,13 @@ class Pipe:
         self.base_position = self.height + self.range
 
     def move(self):
-        self.x -= self.speed
+        self.x -= Pipe.speed
 
     def draw(self, window):
         window.blit(self.top_pipe, (self.x, self.top_position))
         window.blit(self.pipe_base, (self.x, self.base_position))
 
-    def colidir(self, bird):
+    def colide(self, bird):
         bird_mask = bird.get_mask()
         topo_mask = pygame.mask.from_surface(self.top_pipe)
         base_mask = pygame.mask.from_surface(self.pipe_base)
@@ -121,10 +126,8 @@ class Pipe:
         top_point = bird_mask.overlap(topo_mask, top_range)
         base_point = bird_mask.overlap(base_mask, base_range)
 
-        if base_point or top_point:
-            return True
-        else:
-            return False
+        return bool(base_point or top_point)
+
 
 class Ground:
     speed = 5
@@ -159,12 +162,29 @@ def draw_window(window, birds, pipes, ground, score):
 
     text = score_font.render(f"Pontuação: {score}", 1, (255, 255, 255))
     window.blit(text, (window_width - 10 - text.get_width(), 10))
+    if ai_playing:
+        texto = score_font.render(f"Geração: {generation}", 1, (255, 255, 255))
+        window.blit(texto, (10, 10))
+
     ground.draw(window)
     pygame.display.update()
 
 
-def main():
-    birds = [Bird(230, 350)]
+def main(genomes, config):
+    global generation
+    generation += 1
+    if ai_playing:
+        redes = []
+        genomes_list = []
+        birds = []
+        for _, genome in genomes:
+            rede = neat.nn.FeedForwardNetwork.create(genome, config)
+            redes.append(rede)
+            genome.fitness = 0
+            genomes_list.append(genome)
+            birds.append(Bird(230, 350))
+    else:
+        birds = [Bird(230, 350)]
     ground = Ground(730)
     pipes = [Pipe(700)]
     window = pygame.display.set_mode((window_width, window_height))
@@ -173,28 +193,57 @@ def main():
 
     running = True
     while running:
-        clock.tick(30)
+        clock.tick(45)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 pygame.quit()
                 quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    for bird in birds:
-                        bird.jump()
+            if not ai_playing:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        for bird in birds:
+                            bird.jump()
 
-        for bird in birds:
+        if len(pipes) == 0:
+            pipes.append(Pipe(window_width))
+
+        pipe_index = 0
+        if len(birds) > 0:
+            for i, pipe in enumerate(pipes):
+                if pipe.x + pipe.top_pipe.get_width() > birds[0].x:
+                    pipe_index = i
+                    break
+        else:
+            running = False
+            break
+
+        for i, bird in enumerate(birds):
             bird.move()
+            if ai_playing:
+                genomes_list[i].fitness += 0.1
+                output = redes[i].activate((
+                    bird.y,
+                    abs(bird.y - pipes[pipe_index].height),
+                    abs(bird.y - pipes[pipe_index].base_position)
+                ))
+                if output[0] > 0.5:
+                    bird.jump()
+
         ground.move()
 
         add_pipe = False
         remove_pipes = []
+
         for pipe in pipes:
             for i, bird in enumerate(birds):
-                if pipe.colidir(bird):
+                if pipe.colide(bird):
                     birds.pop(i)
+                    if ai_playing:
+                        genomes_list[i].fitness -= 1
+                        genomes_list.pop(i)
+                        redes.pop(i)
                 if not pipe.passthrow and bird.x > pipe.x:
                     pipe.passthrow = True
                     add_pipe = True
@@ -204,15 +253,51 @@ def main():
 
         if add_pipe:
             points += 1
-            pipes.append(Pipe(600))
+
+            velocidade_base = 5
+            distancia_base = 140
+            multiplicador = 1.03
+
+            Pipe.speed = velocidade_base * (multiplicador ** points)
+            distancia_entre_pipes = int(distancia_base * (multiplicador ** points))
+
+            pipes.append(Pipe(window_width + distancia_entre_pipes))
+
+            if ai_playing:
+                for genome in genomes_list:
+                    genome.fitness += 5
+
         for pipe in remove_pipes:
             pipes.remove(pipe)
 
         for i, bird in enumerate(birds):
             if (bird.y + bird.image.get_height()) > ground.y or bird.y < 0:
                 birds.pop(i)
+                if ai_playing:
+                    genomes_list.pop(i)
+                    redes.pop(i)
 
         draw_window(window, birds, pipes, ground, points)
 
+
+def run(config_path):
+    config = neat.config.Config(neat.DefaultGenome,
+                                neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet,
+                                neat.DefaultStagnation,
+                                config_path)
+
+    populacao = neat.Population(config)
+    populacao.add_reporter(neat.StdOutReporter(True))
+    populacao.add_reporter(neat.StatisticsReporter())
+
+    if ai_playing:
+        populacao.run(main, 100)
+    else:
+        main(None, None)
+
+
 if __name__ == '__main__':
-    main()
+    path = os.path.dirname(__file__)
+    path_config = os.path.join(path, 'configs.txt')
+    run(path_config)
